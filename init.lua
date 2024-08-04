63644a0e145e1669dce5d2e3e9a2f0c0a252feed
RadtkeJakub <jakubradtke93@gmail.com>
2024-08-04 22:21:18 +0200

    changes for background and tree

--- init.lua
+++ init.lua
@@ -41,7 +41,7 @@ vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
 -- Keep signcolumn on by default
-vim.opt.signcolumn = "yes"
+vim.opt.signcolumn = "no"
 
 -- Decrease update time
 vim.opt.updatetime = 250
@@ -65,6 +65,9 @@ vim.opt.inccommand = "split"
 
 -- Show which line your cursor is on
 vim.opt.cursorline = true
+
+-- Transparent Background
+vim.api.nvim_set_hl(0, "Normal", { guibg = NONE, ctermbg = NONE })
 
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 15
@@ -698,13 +701,13 @@ require("lazy").setup({
 		-- change the command in the config to whatever the name of that colorscheme is.
 		--
 		-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-		"folke/tokyonight.nvim",
+		"navarasu/onedark.nvim",
 		priority = 1000, -- Make sure to load this before all the other start plugins.
 		init = function()
 			-- Load the colorscheme here.
 			-- Like many other themes, this one has different styles, and you could load
 			-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-			vim.cmd.colorscheme("tokyonight-night")
+			vim.cmd.colorscheme("onedark")
 
 			-- You can configure highlights by doing something like:
 			vim.cmd.hi("Comment gui=none")
@@ -845,5 +848,8 @@ require("lazy").setup({
 	},
 })
 
+-- Transparent Background
+vim.api.nvim_set_hl(0, "Normal", { guibg = NONE, ctermbg = NONE })
+vim.api.nvim_set_hl(0, "NonText", { guibg = NONE, ctermbg = NONE })
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
