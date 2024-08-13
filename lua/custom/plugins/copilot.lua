df8485802f3017171978591cc51e8f7467098eaa
RadtkeJakub <jakubradtke93@gmail.com>
2024-08-13 13:30:45 +0200

    enable copilot autostart

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -2,6 +2,7 @@ return {
 	"zbirenbaum/copilot.lua",
 	cmd = "Copilot",
 	build = ":Copilot auth",
+	event = "InsertEnter",
 	opts = {
 		suggestion = { enabled = true, auto_trigger = true },
 		panel = { enabled = true },
